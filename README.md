e5c3b06ed156a74a29cfc6da2b6e828927f93054
Pranav Rajesh Jadhav <jhadavpranav@gmail.com>
2023-09-15 15:35:58 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+<div align="center">
+
+![GitHub repo size](https://img.shields.io/github/repo-size/Pranav-Jadhav09/Home-Smart)
+[![Twitter Follow](https://img.shields.io/twitter/follow/Pranav_Jadhav09?style=social)](https://twitter.com/Pranav_Jadhav09)
+
+<br />
+<br />
+
+<img src="./build/assets/application.png" style="width: 100">
+
+<h2 align="center">CodeBank - Website</h2>
+Discover smart AI solutions for your Finance and Stocks trading. Explore our products and services today. Made this projects using Tailwind SS.
+
+<a href="https://home-smart-website.onrender.com/"><strong>➥ Lie Demo</strong></a>
+
+</div>
+
+<br />
+
+## Demo
+
+![Demo Screen](./build/assets/demo.png "Desktop Demo")
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- [Git](https://git-scm.com/downloads "Download Git") must be installed on your operating system.
+
+## Run Locally
+
+To run **Home Smart**, run this command on your git bash:
+
+Linux and macOS:
+
+```bash
+sudo git clone https://github.com/Pranav-Jadhav09/Coe-Bank.git
+```
+
+Windows:
+
+```bash
+git clone https://github.com/Pranav-Jadhav09/Code-Bak.git
+```
+
+## License
+
+MIT
---
+++ build/script.js
@@ -0,0 +1,78 @@
+"use strict";
+
+///////////////////////////////
+//// Header Navbar Menu
+const navbar = document.querySelector("header");
+
+window.onscroll = () => {
+  if (window.scrollY > 100) {
+    navbar.classList.add("bg-color-primary-dark");
+    navbar.classList.add("border-b");
+    navbar.classList.add("border-color-gray");
+  } else {
+    navbar.classList.remove("bg-color-primary-dark");
+    navbar.classList.remove("border-b");
+    navbar.classList.remove("border-color-gray");
+  }
+};
+
+///////////////////////////////
+//// Mobile Navbar Menu
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector("#menu");
+const hlink = document.querySelectorAll("#hlink");
+const faSolid = document.querySelector(".fa-solid");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+  faSolid.classList.toggle("fa-xmark");
+});
+
+hlink.forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+    faSolid.classList.toggle("fa-xmark");
+  });
+});
+
+///////////////////////////////
+//// Testimonial Feature
+const userTexts = document.getElementsByClassName("user-text");
+const userPics = document.getElementsByClassName("user-pic");
+
+function showReview() {
+  for (const userPic of userPics) {
+    userPic.classList.remove("active-pic");
+  }
+  for (const userText of userTexts) {
+    userText.classList.remove("active-text");
+  }
+
+  let i = Array.from(userPics).indexOf(event.target);
+  userPics[i].classList.add("active-pic");
+  userTexts[i].classList.add("active-text");
+}
+
+///////////////////////////////
+//// Flip Card Feature
+const toggleBtn = document.getElementById("toggleBtn");
+
+const card_1_front = document.querySelector("#card_1_front");
+const card_1_back = document.querySelector("#card_1_back");
+
+const card_2_front = document.querySelector("#card_2_front");
+const card_2_back = document.querySelector("#card_2_back");
+
+const card_3_front = document.querySelector("#card_3_front");
+const card_3_back = document.querySelector("#card_3_back");
+
+toggleBtn.addEventListener("change", () => {
+  card_1_front.classList.toggle("-rotate-y-180");
+  card_1_back.classList.toggle("rotate-y-180");
+
+  card_2_front.classList.toggle("-rotate-y-180");
+  card_2_back.classList.toggle("rotate-y-180");
+
+  card_3_front.classList.toggle("-rotate-y-180");
+  card_3_back.classList.toggle("rotate-y-180");
+});
